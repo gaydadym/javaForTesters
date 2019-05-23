74775d45fb375c08b318793d3dc83b7f0737227f
m.gaydodim <m.gaydodim@artlogics.ru>
2019-05-23 15:35:32 +0300

    Задание №2. Попытка 2

--- src/main/java/gaidadym/javaForTesters/DistanceBetweenPoints.java
+++ src/main/java/gaidadym/javaForTesters/DistanceBetweenPoints.java
@@ -14,7 +14,7 @@ public class DistanceBetweenPoints {
 
         public static double distance(Point p1,Point p2){
 
-            double dist = Math.sqrt((p2.x-p1.x)+(p2.y-p1.y));
+            double dist = Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
             return dist;
 
         }
--- src/main/java/gaidadym/javaForTesters/Point.java
+++ src/main/java/gaidadym/javaForTesters/Point.java
@@ -2,7 +2,7 @@ package gaidadym.javaForTesters;
 public class Point {
     public int x,y;
     public double distance(Point p2){
-        double dist = Math.sqrt((p2.x-this.x)+(p2.y-this.y));
+        double dist = Math.sqrt(Math.pow((p2.x-this.x),2)+Math.pow((p2.y-this.y),2));
         return dist;
     }
 
