bd86b68f963dd1b0ce8d0ac9a75a84318199ebbe
m.gaydodim <m.gaydodim@artlogics.ru>
2019-07-10 13:58:46 +0300

    Подключил Data provider  к тестам на создание групп и контактов

--- addressbook-web-tests/src/test/java/gaidadym/javaForTesters/addressbook/tests/ContactCreation.java
+++ addressbook-web-tests/src/test/java/gaidadym/javaForTesters/addressbook/tests/ContactCreation.java
@@ -4,32 +4,38 @@ package gaidadym.javaForTesters.addressbook.tests;
 import gaidadym.javaForTesters.addressbook.TestBase;
 import gaidadym.javaForTesters.addressbook.model.ContactData;
 import gaidadym.javaForTesters.addressbook.model.Contacts;
+import gaidadym.javaForTesters.addressbook.model.GroupData;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreation extends TestBase {
-    @Test
-    public void testContactCreation() throws Exception {
-
-        ContactData contact = new ContactData();
-        contact.withNickname("test4");
-        contact.withFirstname("test");
-        contact.withLastname("test");
-        contact.withGroup("test2");
-        contact.withAddress("Test4");
-        contact.withEmail("test@test.com");
-        contact.withEmail2("test@test.com");
-        contact.withEmail3("test@test.com");
-        contact.withHomePhone("48421654");
-        contact.withMobilePhone("48421653");
-        contact.withWorkPhone("48421651");
-        contact.withMiddlename("Testovich");
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src\\test\\resources\\screen.jpg");
-        contact.withPhoto(photo);
+        list.add(new Object[]{new ContactData().withNickname("Test 1").withFirstname("Header 1").withLastname("Footer 1")
+                .withGroup("test2").withAddress("Test4").withEmail("test@test.com").withEmail2("test2@test.com")
+                .withEmail3("test3@test.com").withHomePhone("1111111").withMobilePhone("222222").withWorkPhone("33333")
+                .withMiddlename("Testovich").withPhoto(photo)});
+        list.add(new Object[]{new ContactData().withNickname("Test 2").withFirstname("Header 2").withLastname("Footer 2")
+                .withGroup("test2").withAddress("Test2").withEmail("test@test2.com").withEmail2("test2@test2.com")
+                .withEmail3("test3@test2.com").withHomePhone("21111111").withMobilePhone("3222222").withWorkPhone("433333")
+                .withMiddlename("Testovich2").withPhoto(photo)});
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) throws Exception {
+
         Contacts before = app.contact().all();
         app.contact().create(contact);
         assertThat(app.contact().count(),equalTo(before.size()+1));
--- addressbook-web-tests/src/test/java/gaidadym/javaForTesters/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests/src/test/java/gaidadym/javaForTesters/addressbook/tests/GroupCreation.java
@@ -3,16 +3,30 @@ package gaidadym.javaForTesters.addressbook.tests;
 import gaidadym.javaForTesters.addressbook.model.GroupData;
 import gaidadym.javaForTesters.addressbook.TestBase;
 import gaidadym.javaForTesters.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreation extends TestBase {
-    @Test
-    public void testGroupCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("Test 1").withHeader("Header 1").withFooter("Footer 1")});
+        list.add(new Object[]{new GroupData().withName("Test 2").withHeader("Header 2").withFooter("Footer 2")});
+        list.add(new Object[]{new GroupData().withName("Test 3").withHeader("Header 3").withFooter("Footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group)  {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2").withHeader("test2").withFooter("test2");
         app.group().create(group);
         assertThat(app.group().count(),equalTo(before.size()+1));
         Groups after = app.group().all();
