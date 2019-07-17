4fcb75e5c2f0a36e65338b1ee6ad144e957f7588
m.gaydodim <m.gaydodim@artlogics.ru>
2019-07-17 11:05:03 +0300

    Добавлен модуль для тестов Mantis

---
+++ mantis-tests/src/test/java/gaidadym/javaForTesters/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,71 @@
+package gaidadym.javaForTesters.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+
+
+    public WebDriver wd;
+
+    private String browser;
+
+
+    public ApplicationManager(String browser)  {
+        this.browser = browser;
+        properties = new Properties();
+
+    }
+
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+        if (browser.equals(BrowserType.CHROME)){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--kiosk");
+            wd = new ChromeDriver(options);
+        }
+        else {
+            wd = new FirefoxDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+
+        wd.get(properties.getProperty("web.baseUrl"));
+
+
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
+
+
+
+}
+
---
+++ mantis-tests/src/test/java/gaidadym/javaForTesters/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package gaidadym.javaForTesters.mantis.tests;
+
+import gaidadym.javaForTesters.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    public static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
